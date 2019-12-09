246caaf49411208396127f76324b4323124f5f5d
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2019-12-09 19:17:02 +0200

    -

--- RTScript.Editor/UI/src/App.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var preact_1 = require("preact");
-exports.default = (function (_) { return (preact_1.h("div", null,
-    preact_1.h("p", null, "App works!"))); });
